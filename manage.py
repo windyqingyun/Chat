c57582c703fd86144e20d22c2e888859d660e5a1
yaocai <yaocai>
2017-09-25 09:02:30 +0800

    增加debug配置

--- manage.py
+++ manage.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python
 #coding=utf-8
 from flask.ext.script import Manager
-
-
-
 from app import createApp
 
 app = createApp()
 manager = Manager(app)
-
+app.config.from_object(DevConfig)
 
 if __name__ == "__main__":
     manager.run()
